732009ff387a78444efa9dc11f511953938a44c1
marcelo <altafin@uel.br>
2022-03-14 09:01:07 -0300

    Módulo 08 - Aula 03

---
+++ src/pages/DetailOrder.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+
+    <div class="row my-4">
+      <h1 class="title-tenant">Detalhes do Pedido <strong>{{ identify }}</strong></h1>
+    </div>
+
+    <div class="row my-4">
+      <div class="col-sm-12">
+        <ul>
+          <li>Número: {{ order.identify }}</li>
+          <li>Total: R$ {{ order.total }}</li>
+          <li>Data: {{ order.date }}</li>
+          <li>Status: <span class="badge bg-success">{{ order.status }}</span></li>
+          <li v-if="order.table.identify">
+            Mesa:
+            <ul>
+              <li>Identificador: {{ order.table.identify }}</li>
+              <li>DescriÃ§Ã£o: {{ order.table.description }}</li>
+            </ul>
+          </li>
+          <li>
+            Cliente:
+            <ul v-if="order.client.name">
+              <li>Nome: {{ order.client.name }}</li>
+              <li>E-mail: {{ order.client.email }}</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div> <!-- details order -->
+
+    <hr>
+
+    <div class="row my-4">
+      <div class="col-12">
+        <h4>Produtos</h4>
+      </div>
+
+      <div class="col-6 my-1">
+        <div class="restaurant-card">
+          <a href="vitrine-tenant.html"><img class="card-img-top" src="imgs/pizza.png" alt=""></a>
+          <div class="restaurant-card-body">
+            <h5>Pizza</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 my-1">
+        <div class="restaurant-card">
+          <a href="vitrine-tenant.html"><img class="card-img-top" src="imgs/pizza.png" alt=""></a>
+          <div class="restaurant-card-body">
+            <h5>Pizza</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 my-1">
+        <div class="restaurant-card">
+          <a href="vitrine-tenant.html"><img class="card-img-top" src="imgs/pizza.png" alt=""></a>
+          <div class="restaurant-card-body">
+            <h5>Pizza</h5>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- products order -->
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['identify'],
+
+  created() {
+
+  },
+
+  data() {
+    return {
+      order: {
+        identify: '',
+        date: '',
+        status: '',
+        total: '',
+        table: {
+          identify: '',
+          description: '',
+        },
+        client: {
+          name: '',
+          email: '',
+        },
+        company: {
+          name: '',
+          image: '',
+        },
+        products: [],
+        evaluations: [],
+      }
+    }
+  }
+}
+</script>
